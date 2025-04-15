b481e769bddb22a33b20f9d2494d80aec1c204d9
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2025-04-15 13:24:10 +0530

    - Made Muscle group for female also

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-black py-6 mt-10">
+        <footer className="bg-blue-900 py-6 mt-10">
           <div className="container mx-auto text-center">
             <p className="text-white text-sm">
               © {new Date().getFullYear()} Body Shody. All rights reserved.
