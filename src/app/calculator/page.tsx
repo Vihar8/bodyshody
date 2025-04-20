087b4a71a59e19cd5d8bf27511d6e944a6e184b0
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2025-04-20 13:18:55 +0530

    - Final

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -8,12 +8,14 @@ export default function Home() {
     <>
      <Navbar /> 
     <div className="main-container">
-        
       <section className="bmi-section">
         <h1>BODY MASS INDEX (BMI)</h1>
         <p>Body mass index (BMI) is a measure of body fat based on height and weight that applies to adult men and women. Calculate your BMI to determine your goal.</p>
         <BMICalculator />
       </section>
+      <br/>
+      <hr/>
+      <br/>
       <section className="calorie-section">
         <h1>CALORIC NEEDS</h1>
         <p>The Calorie Calculator can be used to estimate the number of calories a person needs to consume each day. This calculator can also provide some simple guidelines for gaining or losing weight.</p>
